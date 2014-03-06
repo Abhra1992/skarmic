016d27221306bdfac42045ed59c2289842217870
Abhra Basak <abhra.1992@live.com>
2014-03-06 17:47:00 +0530

    Skarmic Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skarmic::Application.config.secret_key_base = '8deebf616fa4cfcb0875c34dac21d4aaf46708cc61636ce0a3064ee34b961f862c9986dcb7dc9a3909d4924ef0734e09a0d05ea326bd491dbf280cfe53b7c061'
