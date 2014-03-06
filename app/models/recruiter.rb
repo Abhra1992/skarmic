2ef084739985982fb6ca81b42922a577c304f500
Abhra Basak <abhra.1992@live.com>
2014-03-07 02:54:04 +0530

    Create Recruiter Model

---
+++ app/models/recruiter.rb
@@ -0,0 +1,14 @@
+class Recruiter < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable
+  belongs_to :company
+
+  validates :username, presence: true, uniqueness: true
+
+  def name
+    "%s %s" % [fname, lname]
+  end
+  alias to_s name
+end
