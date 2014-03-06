4e43cddb65774dd9924d28d37217855ac2f28804
Abhra Basak <abhra.1992@live.com>
2014-03-06 19:17:12 +0530

    Set Up Contact Us Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Skarmic::Application.routes.draw do
+  get "pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
