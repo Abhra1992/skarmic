037f8ac52128ae97eb6fbaa4e8a3afa08747694e
Abhra Basak <abhra.1992@live.com>
2014-03-08 21:07:00 +0530

    Sign In Links added

--- config/routes.rb
+++ config/routes.rb
@@ -58,9 +58,9 @@ Skarmic::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  # namespace :candidate do
-  #   resources :applications, only: [:index, :show, :create, :destroy]
-  # end
+  namespace :candidate do
+    resources :applications, only: [:index, :show, :create, :destroy]
+  end
 
   namespace :recruiter do
     resources :applications, only: :show do
