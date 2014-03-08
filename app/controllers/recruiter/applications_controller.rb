e14412a50439c67e44754012a85353d007a43804
Abhra Basak <abhra.1992@live.com>
2014-03-08 19:33:48 +0530

    Security fix

--- app/controllers/recruiter/applications_controller.rb
+++ app/controllers/recruiter/applications_controller.rb
@@ -1,14 +1,14 @@
 class Recruiter::ApplicationsController < ApplicationController
-  # before_action :authenticate_recruiter!
+  before_action :authenticate_recruiter!
 
   respond_to :js
 
   def show
-    respond_with(@application = Application.find(params[:id]))
+    respond_with(@application = current_recruiter.applications.find(params[:id]))
   end
 
   def rate
-    @application = Application.find params[:id] # current_recruiter.applications
+    @application = current_recruiter.applications.find params[:id]
     @rating = params[:rating]
     @application.rating = @rating
     if @application.save
