b3e729d91a379b4960cc7f49ed33e517a24fc05a
Abhra Basak <abhra.1992@live.com>
2014-03-09 19:48:52 +0530

    Functional Messages for Recruiter

---
+++ app/controllers/recruiter/messages_controller.rb
@@ -0,0 +1,21 @@
+class Recruiter::MessagesController < ApplicationController
+  before_action :authenticate_recruiter!
+  respond_to :js
+
+  def create
+    @application = Application.find params[:application_id]
+    @message = @application.messages.build(body: params[:message][:body], messager: current_recruiter)
+    if @message.save
+      flash[:notice] = "Successfully Posted Message"
+    end
+    respond_with @message
+  end
+
+  def destroy
+    @message = Message.find params[:id]
+    if @message.destroy
+      flash[:notice] = "Message Deleted"
+    end
+    respond_with @message
+  end
+end
---
+++ test/helpers/recruiter/messages_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Recruiter::MessagesHelperTest < ActionView::TestCase
+end
